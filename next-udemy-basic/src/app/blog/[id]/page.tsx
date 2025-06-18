91bb1408258fccb19decf269196879d0d5519598
mshtwtnb0219 <b77yellow@gmail.com>
2025-06-18 21:35:49 +0900

    app router

---
+++ next-udemy-basic/src/app/blog/[id]/page.tsx
@@ -0,0 +1,20 @@
+// 動的ルーティング
+//　フォルダ名は[]で作成　関数は非同期関数で作成
+// 非同期処理はpromise型(非同期処理を安全に記載する)でジェネリクス型で抽象化
+
+type Params = {
+    params: Promise<{
+        id: string
+    }>
+}
+
+export default async function page({ params }: Params) {
+
+    // console.log(params);
+    const { id } = await params // 分割代入
+    return (
+        <div>
+            ブログID：{id}
+        </div>
+    )
+}
